679a1ef8791623ce9014098d43f84ac30cb956fd
wangtaotao <tao_wang1111@163.com>
2021-11-20 17:59:40 +0800

    第er次提交

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -6,5 +6,7 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("woaixifu ");
+        System.out.println("woasixifu ");
+        System.out.println("woaixifu ");
     }
 }
